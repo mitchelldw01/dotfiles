a895bcad66c22d2d36782100e6a024392baed4f0
Mitchell Wilson <mitchelldw01@gmail.com>
2024-02-01 12:17:50 -0600

    Add script for creating symlinks

---
+++ create-links.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -sfn ~/dotfiles/kitty ~/.config/kitty
+ln -sfn ~/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/dotfiles/mitchell.zsh-theme ~/.oh-my-zsh/themes/mitchell.zsh-theme
+
