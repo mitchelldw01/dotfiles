c5ca8b52dd3f88ffb79badd0e0b5682e4eb7c388
Mitchell Wilson <mitchelldw01@gmail.com>
2024-02-02 00:00:58 -0600

    Use native harpoon UI instead of telescope

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -63,30 +63,10 @@ vim.keymap.set("n", "<leader>dw", ":Trouble workspace_diagnostics<CR>", {})
 
 -- harpoon
 local harpoon = require("harpoon")
-local conf = require("telescope.config").values
-
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-			initial_mode = "normal",
-		})
-		:find()
-end
 
 vim.keymap.set("n", "<leader>ao", function()
-	toggle_telescope(harpoon:list())
-end, { desc = "Open harpoon window" })
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
 vim.keymap.set("n", "<leader>aa", function()
 	harpoon:list():append()
--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,6 +3,10 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("harpoon").setup()
+		require("harpoon").setup({
+			settings = {
+				save_on_toggle = true,
+			},
+		})
 	end,
 }
