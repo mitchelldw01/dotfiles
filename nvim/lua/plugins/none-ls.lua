ab5f367e60645d9cd0ee6144b27774642ed2274f
Mitchell Wilson <mitchelldw01@gmail.com>
2024-02-09 19:04:06 -0600

    Only load ESLint and Prettier when config files are present

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,6 +4,24 @@ return {
 		local null_ls = require("null-ls")
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+		local function has_config_file(filename)
+			local function exists_in_dir(dir)
+				return vim.fn.filereadable(dir .. "/" .. filename) ~= 0
+			end
+			local cwd = vim.fn.getcwd()
+			while cwd do
+				if exists_in_dir(cwd) then
+					return true
+				end
+				local parent = vim.fn.fnamemodify(cwd, ":h")
+				if parent == cwd then
+					break
+				end
+				cwd = parent
+			end
+			return false
+		end
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
@@ -11,11 +29,35 @@ return {
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.formatting.eslint_d,
-				null_ls.builtins.code_actions.eslint_d,
+				null_ls.builtins.formatting.golines,
+				null_ls.builtins.diagnostics.eslint_d.with({
+					condition = function()
+						return has_config_file(".eslintrc.js")
+							or has_config_file(".eslintrc.cjs")
+							or has_config_file(".eslintrc.json")
+					end,
+				}),
+				null_ls.builtins.formatting.eslint_d.with({
+					condition = function()
+						return has_config_file(".eslintrc.js")
+							or has_config_file(".eslintrc.cjs")
+							or has_config_file(".eslintrc.json")
+					end,
+				}),
+				null_ls.builtins.code_actions.eslint_d.with({
+					condition = function()
+						return has_config_file(".eslintrc.js")
+							or has_config_file(".eslintrc.cjs")
+							or has_config_file(".eslintrc.json")
+					end,
+				}),
 				null_ls.builtins.formatting.prettier.with({
 					command = "prettierd",
+					condition = function()
+						return has_config_file(".prettierrc")
+							or has_config_file(".prettierrc.js")
+							or has_config_file("prettier.config.js")
+					end,
 				}),
 			},
 			on_attach = function(client, bufnr)
