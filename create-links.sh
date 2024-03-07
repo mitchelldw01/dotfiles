28bdde283d378e8fae66ac1bfbc042fabaa6ec33
Mitchell Wilson <mitchelldw01@gmail.com>
2024-03-06 18:47:35 -0600

    Switch to Wezterm from Kitty

--- create-links.sh
+++ create-links.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ln -sfn ~/dotfiles/kitty ~/.config/kitty
 ln -sfn ~/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 ln -sfn ~/dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/dotfiles/mitchell.zsh-theme ~/.oh-my-zsh/themes/mitchell.zsh-theme
 
