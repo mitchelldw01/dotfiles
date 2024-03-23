23c76de5eb341fbe3649d54026941230391719c7
Mitchell Wilson <mitchelldw01@gmail.com>
2024-03-22 21:06:15 -0500

    Setup tab width for zig

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,15 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "build.zig.zon",
+	callback = function()
+		vim.bo.tabstop = 4
+		vim.bo.softtabstop = 4
+		vim.bo.shiftwidth = 4
+	end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "markdown",
 	callback = function()
