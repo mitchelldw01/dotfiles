0526fbc0ad1e3af1183576b219d2738630a38319
Mitchell Wilson <mitchelldw01@gmail.com>
2024-07-03 20:08:27 -0500

    Tweak some formatting

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,3 @@
--- Plug 'nvim-lua/plenary.nvim' " don't forget to add this one if you don't have it yet!
--- Plug 'ThePrimeagen/harpoon'
 return {
 	"ThePrimeagen/harpoon",
 	dependencies = { "nvim-lua/plenary.nvim" },
