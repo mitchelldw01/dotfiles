4a4dee02d43eda41c94243c6b3a0d1be59b6cdb1
Mitchell Wilson <mitchelldw01@gmail.com>
2024-07-31 16:19:49 -0500

    Make it easy to add telescope ignore patterns

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,46 +13,32 @@ return {
 		},
 	},
 	config = function()
+		local ignore_patterns = {
+			"**/.git/",
+			"**/node_modules/",
+			"**/*_templ.*",
+			"build/*",
+			".cache/*",
+			"vendor/*",
+			".zig-cache/*",
+		}
+
+		local ignore_flags = {}
+		for _, pattern in ipairs(ignore_patterns) do
+			table.insert(ignore_flags, "--glob")
+			table.insert(ignore_flags, "!" .. pattern)
+		end
+
 		require("telescope").setup({
 			pickers = {
 				find_files = {
 					previewer = false,
-					find_command = {
-						"rg",
-						"--files",
-						"--hidden",
-						"--no-ignore-vcs",
-						"--glob",
-						"!**/.git/",
-						"--glob",
-						"!**/node_modules/",
-						"--glob",
-						"!**/*_templ.*",
-						"--glob",
-						"!build/*",
-						"--glob",
-						"!.cache/*",
-						"--glob",
-						"!vendor/*",
-					},
+					find_command = vim.list_extend({ "rg", "--files", "--hidden", "--no-ignore-vcs" }, ignore_flags),
 				},
 				live_grep = {
-					additional_args = {
-						"--hidden",
-						"--no-ignore-vcs",
-						"--glob",
-						"!**/node_modules/",
-						"--glob",
-						"!**/.git/",
-						"--glob",
-						"!**/*_templ.*",
-						"--glob",
-						"!build/*",
-						"--glob",
-						"!.cache/*",
-						"--glob",
-						"!vendor/*",
-					},
+					additional_args = function()
+						return vim.list_extend({ "--hidden", "--no-ignore-vcs" }, ignore_flags)
+					end,
 				},
 				buffers = {
 					sort_mru = true,
