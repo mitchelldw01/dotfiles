e1fa5be9c9c0a70dcffba8914473eed144aeca98
Mitchell Wilson <mitchelldw01@gmail.com>
2024-08-21 22:52:39 -0500

    Fix duplicate commit notification bug

--- .config/nvim/lua/plugins/nvim-notify.lua
+++ .config/nvim/lua/plugins/nvim-notify.lua
@@ -2,7 +2,6 @@ return {
 	"rcarriga/nvim-notify",
 	config = function()
 		local notify = require("notify")
-		local timer = vim.loop.new_timer()
 		local last_notified_hash = nil
 
 		local function get_current_branch(callback)
@@ -21,50 +20,44 @@ return {
 			vim.system({ "git", "rev-list", "HEAD..origin/" .. branch, "--count" }, { text = true }, callback)
 		end
 
-		local function maybe_send_notification(unpulled_commits, current_remote_hash, current_branch)
+		local function maybe_send_notification(unpulled_commits, current_branch)
 			if unpulled_commits and unpulled_commits > 0 then
-				last_notified_hash = current_remote_hash
-				notify(unpulled_commits .. " unpulled commit(s) on " .. current_branch, "info", {
-					title = "Git",
-				})
+				notify(unpulled_commits .. " unpulled commit(s) on " .. current_branch, "info", { title = "Git" })
 			end
 		end
 
 		local function check_unpulled_commits()
 			get_current_branch(function(branch_result)
-				if branch_result.code ~= 0 then
-					return
-				end
-
 				local current_branch = branch_result.stdout:match("^%s*(.-)%s*$")
+
 				get_remote_hash(current_branch, function(result)
-					if result.code ~= 0 then
-						return
-					end
-
 					local current_remote_hash = result.stdout:match("^%s*(.-)%s*$")
+
 					if last_notified_hash == current_remote_hash then
+						vim.defer_fn(function()
+							check_unpulled_commits()
+						end, 10000)
 						return
 					end
 
-					fetch_origin(function(fetch_result)
-						if fetch_result.code ~= 0 then
-							return
-						end
+					last_notified_hash = current_remote_hash
 
+					fetch_origin(function()
 						get_unpulled_commit_count(current_branch, function(rev_result)
-							if rev_result.code ~= 0 then
-								return
-							end
-
 							local unpulled_commits = tonumber(rev_result.stdout)
-							maybe_send_notification(unpulled_commits, current_remote_hash, current_branch)
+							maybe_send_notification(unpulled_commits, current_branch)
+
+							vim.defer_fn(function()
+								check_unpulled_commits()
+							end, 10000)
 						end)
 					end)
 				end)
 			end)
 		end
 
-		timer:start(0, 10000, vim.schedule_wrap(check_unpulled_commits))
+		fetch_origin(function()
+			check_unpulled_commits()
+		end)
 	end,
 }
